9bfbb9a48102acb65b1efb453fe9da440f3998d2
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-24 23:22:47 +0530

    done

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const mainRoute = require("./routes/mainRoute")
 const cors = require("cors")
 
-const apiProxy = createProxyMiddleware('/api', {
-    target: 'https://todo-backend-p60w.onrender.com',
-    changeOrigin: true,
-  });
-  app.use(apiProxy);
+// const apiProxy = createProxyMiddleware('/api', {
+//     target: 'https://todo-backend-p60w.onrender.com',
+//     changeOrigin: true,
+//   });
+//   app.use(apiProxy);
 app.use(express.json())
 app.use(cors())
-app.use('/api/user', mainRoute)
+
+app.use('/user', mainRoute)
 
 
 app.listen(5000,()=>{
