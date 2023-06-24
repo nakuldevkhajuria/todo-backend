755e8d23a636624ec598d1dab16e62b710276d17
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-25 00:59:14 +0530

    origin:* incors

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+
 
 const app = express();
 const mainRoute = require("./routes/mainRoute")
-const cors = require("cors");
-const { createUser, loginUser, forgetPassword } = require("./controllers/UserCtrl");
+const cors = require("cors")
 
-// const apiProxy = createProxyMiddleware('/api', {
-//     target: 'https://todo-backend-p60w.onrender.com',
-//     changeOrigin: true,
-//   });
-//   app.use(apiProxy);
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 app.get('/',(req,res)=>{
     res.send('helo')
 })
 
-// app.use('/user', mainRoute)
-app.post('/user/register',createUser)
-app.post("/user/login",loginUser)
-app.put("/user/:mobile",forgetPassword)
+app.use('/user', mainRoute)
+
 
 app.listen(5000,async()=>{
     console.log('server is running on port 5000')
